264718038dcf1a785b566e65f2b9ea015ffbe6d4
Aliev Ilya <aliev.ilia@yandex.ru>
2020-02-15 04:17:28 +0300

    all part 4
    api пока не работает

--- Ras/part4/models.py
+++ Ras/part4/models.py
@@ -2,8 +2,8 @@ from django.db import models
 
 class Part4(models.Model):
     part4_id = models.AutoField(primary_key=True)
-    part4_line = models.FloatField(default = 0)
-    part4_canal = models.FloatField(default = 0)
-    part4_number_val = models.FloatField(default = 0)
+    line_selected = models.FloatField(default = 0)
+    canal_selected = models.FloatField(default = 0)
+    number_val = models.FloatField(default = 0)
 
 
--- Ras/part4/urls.py
+++ Ras/part4/urls.py
@@ -6,10 +6,10 @@ from django.conf.urls.static import static
 from django.conf import settings
 
 urlpatterns = [
-    path(r'', TemplateView.as_view(template_name='mainmenu.html')),
-    path('part3/', TemplateView.as_view(template_name='part3.html')),
-    path('part4/', include('part4.urls')),
-
+    path(r'', TemplateView.as_view(template_name='part4.html')),
+    path('page2/', TemplateView.as_view(template_name='Page2.html')),
+    path('page3/', TemplateView.as_view(template_name='Page3.html')),
+    path('page4/', TemplateView.as_view(template_name='Page4.html')),
 ] + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
 
 #path(r'', views.mainmenu, name='mainmenu'),
